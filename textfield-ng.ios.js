30cf04e209e18f359285daedb2f6881879097524
Aleksey Oblozhko <oblozhko@gmail.com>
2017-01-05 17:41:56 +0700

    ios focus event

--- textfield-ng.ios.js
+++ textfield-ng.ios.js
@@ -2,13 +2,49 @@ var common = require("./textfield-ng-common");
 
 require("utils/module-merge").merge(common, module.exports);
 
+var UITextFieldDelegateImpl = (function (_super) {
+    __extends(UITextFieldDelegateImpl, _super);
+    function UITextFieldDelegateImpl() {
+        _super.apply(this, arguments);
+    }
+    UITextFieldDelegateImpl.initWithOwner = function (owner) {
+        var delegate = UITextFieldDelegateImpl.new();
+        delegate._owner = owner;
+        return delegate;
+    };
+    UITextFieldDelegateImpl.prototype.textFieldDidBeginEditing = function (searchBar) {
+        var owner = this._owner.get();
+        if (!owner) {
+            return;
+        }
+        owner._emit('focus');
+    };
+    UITextFieldDelegateImpl.prototype.textFieldDidEndEditing = function (searchBar) {
+        var owner = this._owner.get();
+        if (!owner) {
+            return;
+        }
+        //owner._emit('focus');
+    };
+    UITextFieldDelegateImpl.ObjCProtocols = [UITextFieldDelegate];
+    return UITextFieldDelegateImpl;
+}(NSObject));
+
 var TextFieldNg = (function (_super) {
     global.__extends(TextFieldNg, _super);
     function TextFieldNg() {
         _super.apply(this, arguments);
         this._ios = new UITextField();
+        this._delegate = UITextFieldDelegateImpl.initWithOwner(new WeakRef(this));
     }
-
+    TextFieldNg.prototype.onLoaded = function () {
+        _super.prototype.onLoaded.call(this);
+        this._ios.delegate = this._delegate;
+    };
+    TextFieldNg.prototype.onUnloaded = function () {
+        this._ios.delegate = null;
+        _super.prototype.onUnloaded.call(this);
+    };
     Object.defineProperty(TextFieldNg.prototype, "ios", {
         get: function () {
             return this._ios;
